05a39ce994ca8d5bd6a1c3f649050d6b689714a6
AndriusZimkus <and.zimkus@gmail.com>
2022-01-27 22:17:49 +0200

    1 commit

---
+++ Java_Learning_1/src/main/java/HelloWorld/HelloWorld.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package HelloWorld;
+
+/**
+ *
+ * @author azimk
+ */
+
+public class HelloWorld {
+    
+    public static void main(String[] args){
+        System.out.println("Hello World!"); 
+        int speedLimit = 80;
+        boolean flag1 = true;
+        float myFloat = 3.4F;
+        System.out.println(myFloat);    // prints 3.4
+    }
+}
