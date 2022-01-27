66b1c4a32de3edebdb8487d35ab50ce973f9c750
AndriusZimkus <and.zimkus@gmail.com>
2022-01-27 22:21:03 +0200

    1 commit

--- Java_Learning_1/src/main/java/HelloWorld/HelloWorld.java
+++ Java_Learning_1/src/main/java/HelloWorld/HelloWorld.java
@@ -12,7 +12,7 @@ package HelloWorld;
 public class HelloWorld {
     
     public static void main(String[] args){
-        System.out.println("Hello World!"); 
+        System.out.println("Hello World! Second edition"); 
         int speedLimit = 80;
         boolean flag1 = true;
         float myFloat = 3.4F;
